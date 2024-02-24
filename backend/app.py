b11cac8007a78162fc8add9e82a786a1353943d6
Ssamdeman <samuel.damon73@gmail.com>
2024-02-24 11:12:29 -0500

    testing the evn

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,4 @@
 # Write your Application here
+# Description: This is the main file for the backend of the application. 
+#It contains the main application logic and the API endpoints.
+#most used methods to get all the other files
